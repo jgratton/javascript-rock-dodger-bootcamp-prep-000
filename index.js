a10212664437d1d3d99f37863b28f4a4139c98f6
John Gratton <jgratton@gmail.com>
2018-03-30 16:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function endGame() {
   ROCKS.forEach(function(rock) { rock.remove() });
 
   //keydown.preventDefault();
-  keypress.removeEventListener( 'keydown', moveDodger );
+  keyboardEvent.removeEventListener( 'keydown', moveDodger );
 
   START.innerHTML = 'Play again?';
   START.style.display = 'inline';
